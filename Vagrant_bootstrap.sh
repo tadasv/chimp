a71f7633bdafa15aa51e78bcb239c6fbf0888014
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2013-11-23 21:49:53 -0500

    add vagrant configuration

---
+++ Vagrant_bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install autoconf automake libtool build-essential git valgrind libbsd-dev check vim
+cd /tmp && git clone https://github.com/tadasv/csv_parser.git && cd csv_parser && ./autogen.sh && ./configure && make && sudo make install
+cd /tmp && wget "http://libuv.org/dist/v0.10.19/libuv-v0.10.19.tar.gz" && tar zxf libuv-v0.10.19.tar.gz && cd libuv-v0.10.19 && make && sudo cp -r include/* /usr/local/include && sudo cp libuv.* /usr/local/lib
+sudo ldconfig
