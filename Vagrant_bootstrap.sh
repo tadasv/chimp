1351308bd74564b1a2b568dd9740b57eebe37ef5
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2013-12-06 22:20:20 -0500

    add msgpack-python to vagrant deps

--- Vagrant_bootstrap.sh
+++ Vagrant_bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install autoconf automake libtool build-essential git valgrind libbsd-dev vim cmake libgtest-dev libmsgpack-dev
+sudo apt-get -y install autoconf automake libtool build-essential git valgrind libbsd-dev vim cmake libgtest-dev libmsgpack-dev python-setuptools
+sudo easy_install msgpack-python
 cd /tmp && wget "http://libuv.org/dist/v0.10.19/libuv-v0.10.19.tar.gz" && tar zxf libuv-v0.10.19.tar.gz && cd libuv-v0.10.19 && make && sudo cp -r include/* /usr/local/include && sudo cp libuv.* /usr/local/lib
 cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/ && cd -
 
