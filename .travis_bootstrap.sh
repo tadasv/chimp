1f7a03b3c52016a5c5d230a5fdb136b757307fe3
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2013-12-19 22:10:19 -0500

    update travis/vagrant build deps

--- .travis_bootstrap.sh
+++ .travis_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -qq
-sudo apt-get -qq install autoconf automake libtool build-essential git libbsd-dev libgtest-dev cmake libmsgpack-dev
+sudo apt-get -qq install autoconf automake libtool build-essential git libbsd-dev libgtest-dev cmake libmsgpack-dev libarmadillo-dev libboost-math-dev
 cd /tmp && wget "http://libuv.org/dist/v0.10.19/libuv-v0.10.19.tar.gz" && tar zxf libuv-v0.10.19.tar.gz && cd libuv-v0.10.19 && make && sudo cp -r include/* /usr/local/include && sudo cp libuv.* /usr/local/lib
 cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/ && cd -
 sudo ldconfig
--- Vagrant_bootstrap.sh
+++ Vagrant_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install autoconf automake libtool build-essential git valgrind libbsd-dev vim cmake libgtest-dev libmsgpack-dev python-setuptools
+sudo apt-get -y install autoconf automake libtool build-essential git valgrind libbsd-dev vim cmake libgtest-dev libmsgpack-dev python-setuptools libarmadillo-dev libboost-math-dev
 sudo easy_install msgpack-python
 cd /tmp && wget "http://libuv.org/dist/v0.10.19/libuv-v0.10.19.tar.gz" && tar zxf libuv-v0.10.19.tar.gz && cd libuv-v0.10.19 && make && sudo cp -r include/* /usr/local/include && sudo cp libuv.* /usr/local/lib
 cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/ && cd -
